daf331991e13fcf186f432c80c9623b2ab64b3b8
rahmeesha26 <rahmeeshar91@gmail.com>
2023-05-15 21:56:29 -0400

    palindrome completed

--- solutions.js
+++ solutions.js
@@ -1,2 +1,28 @@
+////////////////////////////////
+// Verbal Questions
+///////////////////////////////
+
 // 1. a parameter is names listed in a function's definition and a argument are values passed to the function
 // 2. a return gives values from a function and console.log prints other languages
+
+///////////////////////////////
+// Palidrome
+///////////////////////////////
+
+const checkPalindrome = {}
+if ("Radar"!== true){
+          console.log(true);
+} else {
+          console.log(false)
+}
+
+const checkPalindrome2 = {}
+if ("Grey" <= false){
+          console.log(false)
+} else {
+          console.log(true)
+}
+
+///////////////////////////////////////
+
+///////////////////////////////////////
