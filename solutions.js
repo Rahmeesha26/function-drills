0863111625075562bc7e45f2be2f2beb75d795b9
rahmeesha26 <rahmeeshar91@gmail.com>
2023-05-15 20:24:50 -0400

    verbal questions completed

---
+++ solutions.js
@@ -0,0 +1,2 @@
+// 1. a parameter is names listed in a function's definition and a argument are values passed to the function
+// 2. a return gives values from a function and console.log prints other languages
